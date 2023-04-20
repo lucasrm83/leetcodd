274b74b636efe641c27633ed03e4327fc64dbe39
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-04-20 12:43:24 -0300

    Palindrome completed with two diferent solutions

--- easy/Palindrome/Palindrome.java
+++ easy/Palindrome/Palindrome.java
@@ -2,9 +2,16 @@ package Palindrome;
 
 public class Palindrome {
     public static void main(String[] args) {
-        int x = 121;
-        for (int i = 0; i <; i++) {
-
+        int x = 2991;
+        if(x<0||(x>0 && x%10==0)){
+            System.out.println(false);
         }
+        int reverso =0;
+        while (x>reverso){
+            reverso = reverso*10 +x%10;
+            x = x/10;
+        }
+        System.out.println(x==reverso||x==reverso/10);
+
     }
 }
---
+++ easy/Palindrome/PalindromeString.java
@@ -0,0 +1,16 @@
+package Palindrome;
+
+public class PalindromeString {
+    public static void main(String[] args) {
+        int x = 121;
+        String s = String.valueOf(x);
+        int n = s.length();
+        for (int i = 0; i <n/2 ; i++) {
+            if (s.charAt(i)!= s.charAt(n-1-i)){
+                return false;
+            }
+
+        }
+        return true;
+    }
+}
