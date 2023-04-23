88061d7407b97ea3060a25c139fb5708ccfa1dde
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-04-22 23:45:30 -0300

    RomanToInteger optimized

---
+++ easy/romantointeger/RomanToIntegerOptimized.java
@@ -0,0 +1,22 @@
+package romantointeger;
+
+public class RomanToIntegerOptimized {
+    public static void main(String[] args) {
+        String s = "MCMXCIV";
+        int ans = 0, num = 0;
+        for (int i = s.length()-1; i >= 0; i--) {
+            switch(s.charAt(i)) {
+                case 'I': num = 1; break;
+                case 'V': num = 5; break;
+                case 'X': num = 10; break;
+                case 'L': num = 50; break;
+                case 'C': num = 100; break;
+                case 'D': num = 500; break;
+                case 'M': num = 1000; break;
+            }
+            if ( num < ans) ans -= num;
+            else ans += num;
+        }
+        System.out.println(ans);
+    }
+}
