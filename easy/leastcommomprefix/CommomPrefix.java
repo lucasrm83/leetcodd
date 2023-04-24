9d326c28e43944c0f179e903bc478ab5791b1836
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-04-24 19:03:50 -0300

    commomprefix

--- easy/leastcommomprefix/CommomPrefix.java
+++ easy/leastcommomprefix/CommomPrefix.java
@@ -1,4 +1,23 @@
 package leastcommomprefix;
 
+import java.util.Arrays;
+
 public class CommomPrefix {
+    public static void main(String[] args) {
+        String [] strs = {"flower","flight","flow"};
+        Arrays.sort(strs);
+        String s1 = strs[0];
+        String s2 = strs[strs.length-1];
+        int idx = 0;
+        while(idx < s1.length() && idx < s2.length()){
+            if(s1.charAt(idx) == s2.charAt(idx)){
+                idx++;
+            } else {
+                break;
+            }
+        }
+        System.out.println(s1.substring(0, idx));
+        //return s1.substring(0, idx);
+    }
+
 }
