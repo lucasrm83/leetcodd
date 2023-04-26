a9b9f1324583bd646622d9ee618629cd4471e4e4
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-04-26 10:32:13 -0300

    ValidParenthesis finished

--- easy/validParenthesis/ValidParenthesis.java
+++ easy/validParenthesis/ValidParenthesis.java
@@ -1,8 +1,24 @@
 package validParenthesis;
 
+import java.util.Stack;
+
 public class ValidParenthesis {
     public static void main(String[] args) {
-        String s = "(){}[]";
+        Stack<Character> ch = new Stack<>();
+        String s = "({[]})";
+        for (char c:s.toCharArray()) {
+            if (c =='('||c=='{'||c=='['){
+                ch.push(c);
+            }
+            else {
+                if (ch.isEmpty() || ch.peek() =='(' && c != ')' ||  ch.peek() =='{' && c != '}' ||  ch.peek() =='[' && c != ']'){
+                    System.out.println(false);
+                }
+                ch.pop();
+            }
+
+        }
+        System.out.println(ch.isEmpty());
 
 
     }
