fc1ceaefb4fcd6e552ce06fc73ae1170e5ed6799
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-04-28 14:23:47 -0300

    mergelists complete

--- easy/mergetwolinkedlists/MergeLists.java
+++ easy/mergetwolinkedlists/MergeLists.java
@@ -2,8 +2,26 @@ package mergetwolinkedlists;
 
 public class MergeLists {
     public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(3);
+        l1.next.next = new ListNode(5);
 
+        ListNode l2 = new ListNode(2);
+        l2.next = new ListNode(4);
+        l2.next.next = new ListNode(6);
+        ListNode result = new ListNode();
+        result = result.mergeTwoLists(l1,l2);
+
+        printList(result);
 
 
     }
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " -> ");
+            head = head.next;
+        }
+        System.out.print("null");
+    }
+
 }
--- easy/mergetwolinkedlists/MergeListsAlt.java
+++
@@ -1,5 +0,0 @@
-package mergetwolinkedlists;
-
-public class MergeListsAlt {
-
-}
