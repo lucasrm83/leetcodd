666d9e8ee84c01e5c95317a200c8e10dc20ccdc1
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-04-30 21:09:04 -0300

    RemoveDuplicates Start

--- easy/removeduplicates/RemoveDuplicates.java
+++ easy/removeduplicates/RemoveDuplicates.java
@@ -9,7 +9,8 @@ public class RemoveDuplicates {
         int k = 0;
 
         for (int i = 0; i <nums.length-1 ; i++) {
-            if (nums[i] != nums[i+1]) {
+            if (nums[i] < nums[i+1]) {
+                nums[i] = nums[k];
                 k++;
             }
 
