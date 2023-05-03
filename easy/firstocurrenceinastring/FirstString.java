07ccfe0241ba607ea9edd360f151cf098db85d72
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-05-02 21:07:25 -0300

    FirstString start

---
+++ easy/firstocurrenceinastring/FirstString.java
@@ -0,0 +1,11 @@
+package firstocurrenceinastring;
+
+public class FirstString {
+    public static void main(String[] args) {
+        String haystack;
+        String needle;
+        if(haystack < needle){
+
+        }
+    }
+}
