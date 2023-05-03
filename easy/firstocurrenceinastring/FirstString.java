530757cdb481858677dc7e384cbf1223bb2d4461
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-05-03 15:05:49 -0300

    FirstString completed

--- easy/firstocurrenceinastring/FirstString.java
+++ easy/firstocurrenceinastring/FirstString.java
@@ -2,10 +2,29 @@ package firstocurrenceinastring;
 
 public class FirstString {
     public static void main(String[] args) {
-        String haystack;
-        String needle;
-        if(haystack < needle){
+        String haystack = "xlasun";
+        String needle="sun";
+        int hay = haystack.length();
+        int need = needle.length();
+
+        if(hay < need){
+            System.out.println("menor");
 
         }
+
+        for (int i = 0; i <=hay-need ; i++) {
+            int j =0;
+            while(j<need && haystack.charAt(i+j)== needle.charAt(j)){
+                j++;
+
+            }
+            if (j==need){
+                System.out.println(i);
+            }
+        }
+        System.out.println("nao tem");
+
+
+
     }
 }
