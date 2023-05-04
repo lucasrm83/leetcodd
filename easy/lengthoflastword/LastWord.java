6ea99b2b84471e3877fc514adf06a559133005c0
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-05-04 12:38:16 -0300

    lastword finished

--- easy/lengthoflastword/LastWord.java
+++ easy/lengthoflastword/LastWord.java
@@ -2,6 +2,26 @@ package lengthoflastword;
 
 public class LastWord {
     public static void main(String[] args) {
+        String s = "   fly me   to   the moon  ";
+        int k =0;
+        int last =0;
+        char ch = ' ';
+        for (int i = 0; i <s.length() ; i++) {
+            if(s.charAt(i) ==ch ){
+                k=0;
+            }
+            else {
+                k++;
+
+            }
+            if (s.charAt(i) != ch) {
+                last = k;
+            }
+
+
+        }
+        System.out.println(last);
+
 
     }
 }
