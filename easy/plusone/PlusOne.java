612d188e0dba6ab30542ee0179d6f26d56f28e63
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-05-06 23:50:00 -0300

    plusone started

--- easy/plusone/PlusOne.java
+++ easy/plusone/PlusOne.java
@@ -3,6 +3,13 @@ package plusone;
 public class PlusOne {
     public static void main(String[] args) {
         int [] digits = {1,2,3};
+        int poiter =0;
+        for (int i = 0; i <digits.length ; i++) {
+            if (i == 9){
+
+            }
+
+        }
 
     }
 }
