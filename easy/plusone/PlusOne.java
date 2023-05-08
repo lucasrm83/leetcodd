ebaa4e826ead52e301829a021c7049f8d9a70ffe
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-05-07 21:05:51 -0300

    plusone started

--- easy/plusone/PlusOne.java
+++ easy/plusone/PlusOne.java
@@ -3,9 +3,11 @@ package plusone;
 public class PlusOne {
     public static void main(String[] args) {
         int [] digits = {1,2,3};
-        int poiter =0;
+        int pointer =0;
         for (int i = 0; i <digits.length ; i++) {
             if (i == 9){
+                pointer=(pointer+i)+i*10;
+
 
             }
 
