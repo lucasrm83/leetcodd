f89938dfbffdd58b66c80986bf2884109d3cb606
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-05-09 15:22:14 -0300

    AddBinary started

---
+++ easy/addbinary/AddBinary.java
@@ -0,0 +1,15 @@
+package addbinary;
+
+public class AddBinary {
+    public static void main(String[] args) {
+        String a = "100";
+        String b = "10";
+        for (int i = 0; i < a.length(); i++) {
+            if(i ==a.charAt(i)){
+
+            }
+
+        }
+
+    }
+}
