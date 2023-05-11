1f7d747bac2314417a449d1066b4f660796bff40
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-05-10 22:22:45 -0300

    AddBinary almost finished

---
+++ easy/addbinary/AddBinaryOptimized.java
@@ -0,0 +1,22 @@
+package addbinary;
+
+public class AddBinaryOptimized {
+    public static void main(String[] args) {
+        String a = "10000";
+        String b = "10000";
+        StringBuilder res = new StringBuilder();
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+        while(i >= 0 || j >= 0){
+            int sum = carry;
+            if(i >= 0) sum += a.charAt(i--) - '0';
+            if(j >= 0) sum += b.charAt(j--) - '0';
+            carry = sum > 1 ? 1 : 0;
+            res.append(sum % 2);
+        }
+        if(carry != 0) res.append(carry);
+        System.out.println(res.reverse().toString());
+    }
+
+}
