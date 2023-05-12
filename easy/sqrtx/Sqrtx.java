ddd0d1684f36162863860f8a605cd0ce621aece6
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-05-12 15:04:09 -0300

    sqrt bad solution finished

--- easy/sqrtx/Sqrtx.java
+++ easy/sqrtx/Sqrtx.java
@@ -2,6 +2,18 @@ package sqrtx;
 
 public class Sqrtx {
     public static void main(String[] args) {
+        int x = 1;
+        if (x ==0) System.out.println(0);
+        for (int i = 1; i <=x ; i++) {
+            if (x/i ==i){
+                System.out.println(i);
+            }
+
+        }
+
+
+
+
 
     }
 }
