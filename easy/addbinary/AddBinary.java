34bb17ec36e7a1632dca3861f896106efc165f6c
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-05-12 12:40:10 -0300

    AddBinary finally finished

--- easy/addbinary/AddBinary.java
+++ easy/addbinary/AddBinary.java
@@ -2,14 +2,30 @@ package addbinary;
 
 public class AddBinary {
     public static void main(String[] args) {
-        String a = "100";
-        String b = "10";
-        for (int i = 0; i < a.length(); i++) {
-            if(i ==a.charAt(i)){
-
+        String a = "11111";
+        String b = "111";
+        StringBuilder res = new StringBuilder();
+        int v1 = a.length()-1;
+        int v2 = b.length()-1;
+        int p = v1>v2 ? v1 : v2;
+        int carry = 0;
+        for (int i = p; i > -1; i--) {
+            int soma = carry;
+            if(v1>=0){
+                soma+=a.charAt(v1) -'0';
+                v1--;
             }
+            if (v2>=0){
+                soma+=b.charAt(v2)-'0';
+                v2--;
+            }
+            carry = soma >1 ? 1:0;
+            System.out.println(soma);
+            res.append(soma%2);
 
         }
+        if (carry !=0) res.append(carry);
+        System.out.println(res.reverse());
 
     }
 }
--- easy/addbinary/AddBinaryOptimized.java
+++ easy/addbinary/AddBinaryOptimized.java
@@ -2,8 +2,8 @@ package addbinary;
 
 public class AddBinaryOptimized {
     public static void main(String[] args) {
-        String a = "10000";
-        String b = "10000";
+        String a = "11111";
+        String b = "111";
         StringBuilder res = new StringBuilder();
         int i = a.length() - 1;
         int j = b.length() - 1;
@@ -13,6 +13,7 @@ public class AddBinaryOptimized {
             if(i >= 0) sum += a.charAt(i--) - '0';
             if(j >= 0) sum += b.charAt(j--) - '0';
             carry = sum > 1 ? 1 : 0;
+            System.out.println(sum);
             res.append(sum % 2);
         }
         if(carry != 0) res.append(carry);
