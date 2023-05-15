fe934f07849b2c9123699c22abe123754af25677
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-05-15 18:04:33 -0300

    sqrt binary finished

--- easy/sqrtx/SqrtxBinary.java
+++ easy/sqrtx/SqrtxBinary.java
@@ -2,7 +2,7 @@ package sqrtx;
 
 public class SqrtxBinary {
     public static void main(String[] args) {
-        int x = 8;
+        int x = 4;
         int low =1;
         int high = x;
         int res =0;
@@ -11,11 +11,11 @@ public class SqrtxBinary {
             int mid = low +(high-low)/2;
 
 
-            if (mid*mid==x){
+            if (x/mid==mid){
                 System.out.println(mid);
                 break;
             }
-            else if (mid*mid>x) {
+            else if (x/mid<mid) {
                 high =mid-1;
 
             }
