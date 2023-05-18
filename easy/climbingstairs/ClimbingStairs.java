4d5b9ce1d63155df8ee3f099439139b0b01a5945
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-05-18 19:48:50 -0300

    ClimbingStairs start

--- easy/climbingstairs/ClimbingStairs.java
+++ easy/climbingstairs/ClimbingStairs.java
@@ -3,6 +3,12 @@ package climbingstairs;
 public class ClimbingStairs {
     public static void main(String[] args) {
         int n = 7;
+        if (n==0){
+
+        }
+        if(n==1){
+
+        }
 
     }
 }
