56101fbdb6cef86038c0c17b396ada5373034a76
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-05-20 15:38:22 -0300

    removeelement revisão

--- easy/removeelement/RemoveElement.java
+++ easy/removeelement/RemoveElement.java
@@ -9,6 +9,11 @@ public class RemoveElement {
         for (int i = 0; i <=nums.length-1 ; i++) {
             if (nums[i] !=val){
                 nums[k] = nums[i];
+                // enquanto o [i] for diferente do var adiciona esse numero na posição k e incrementa k
+                // se [i] for val não vai cair nessa condição, mas como k foi incrementado ele vai
+                // apontar para o proximo indece da ultima ocorrencia de nums[i] != val, ou seja para val
+                // ai quando outro numero diferente de val aparecer vai ser incrementado no indece de val,
+                // removendo ele
                 k++;
             }
 
