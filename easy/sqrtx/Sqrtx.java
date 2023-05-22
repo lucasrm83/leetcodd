341903953aa7c850ad990b45f68b24d31b61dbb3
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-05-22 00:04:36 -0300

    sqrt revisao

--- easy/sqrtx/Sqrtx.java
+++ easy/sqrtx/Sqrtx.java
@@ -3,6 +3,7 @@ package sqrtx;
 public class Sqrtx {
     public static void main(String[] args) {
         int x = 1;
+        //x vai ser o index
         if (x ==0) System.out.println(0);
         for (int i = 1; i <=x ; i++) {
             if (x/i ==i){
