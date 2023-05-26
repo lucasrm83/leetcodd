9a45bffd6048a7e8cb4fbe1e54976d37aa7c07ce
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-05-26 13:25:19 -0300

    search position revisão e versão binaria

--- easy/insertposition/InsertPosition.java
+++ easy/insertposition/InsertPosition.java
@@ -2,9 +2,11 @@ package insertposition;
 
 public class InsertPosition {
     public static void main(String[] args) {
-        int [] nums = {2,3,4,7,8,9};
-        int target = 11;
+        int [] nums = {1,2,3,4,5,6,7,8,9,28,30,35,40,50,60};
+        int target = 37;
+        int it= 0;
         for (int i = 0; i < nums.length; i++) {
+            it++;
             if (target <= nums[i]){
                 System.out.println(i);
 
@@ -13,6 +15,7 @@ public class InsertPosition {
 
 
         }
+        System.out.println("iterations: "+it);
         System.out.println(nums.length);
     }
 }
