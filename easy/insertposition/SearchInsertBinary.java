5183f1a7c5cdedab0ae8876121b0b5a27a8aae61
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-05-26 14:09:46 -0300

    search position revisão e versão binaria

--- easy/insertposition/SearchInsertBinary.java
+++ easy/insertposition/SearchInsertBinary.java
@@ -2,38 +2,26 @@ package insertposition;
 
 public class SearchInsertBinary {
     public static void main(String[] args) {
-        int [] nums = {1,2,3,4,5,6,7,8,9,28,30,35,40,50,60};
-        int [] reff = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14};
-        int target = 37;
+        int [] nums = {1,3,5,6};
+        int target = 2;
         int low =0;
         int high = nums.length-1;
-        int i =0;
+        int ans = nums.length;
         while (low<=high){
-            i++;
-            int mid = (low+high)/2;
+            int mid = low+ (high-low)/2;
             //System.out.println(mid);
             if (nums[mid]==target){
                 System.out.println(mid);
                 break;
-            } else if (nums[mid]<target && nums[mid+1]>target) {
-                System.out.println(mid+1);
-                break;
 
-            } else if (nums[mid]>target && nums[0]==nums[mid]) {
-                System.out.println(mid);
-                break;
-
-            } else if (nums[mid]<target && nums[nums.length-1]==nums[mid]) {
-                System.out.println(mid+1);
-                break;
-
-            } else if (nums[mid]>target) {
-                high =mid-1;
+            } else if (nums[mid]<target) {
+                low = mid+1;
 
             }  else {
-                low = mid+1;
+                ans = mid;
+                high = mid-1;
             }
         }
-        System.out.println("iterations: "+i);
+        System.out.println(ans);
     }
 }
